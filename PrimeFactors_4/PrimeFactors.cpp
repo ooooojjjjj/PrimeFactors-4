82394fcfb33c7dc18ad28a433732f8b82a1f1f27
oj <ozoppp@gmail.com>
2023-06-01 16:01:34 +0900

    PrimeFactors.cpp Refactoring

--- PrimeFactors_4/PrimeFactors.cpp
+++ PrimeFactors_4/PrimeFactors.cpp
@@ -13,8 +13,11 @@ public:
         {
             if (4 == nNumber)
             {
-                result.push_back(2);
-                result.push_back(2);
+                while (nNumber % 2 == 0)
+                {
+                    result.push_back(2);
+                    nNumber /= 2;
+                }
             }
             else
             {
